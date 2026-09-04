190c68447053ff5f4b718c13f5c3c13c1aae99c1
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2026-09-04 10:38:48 +0200

    refactor: remove loop from transform_str func

--- hw_7/task_1.py
+++ hw_7/task_1.py
@@ -1,23 +1,14 @@
 raw_user_record = " 10827 ; aLeXanDer_vLaDimiRov ; mInSk ; ACTIVE "
 
 def transform_str(string_val, sep, divider, prefix):
-    str_arr = string_val.split(sep)
-    
-    for i in range(len(str_arr)):
-        s = str_arr[i].strip().title()
-        
-        if i == 0:
-            s = f"{prefix}{s}"
-        elif i == 1:
-            s = s.replace("_", " ")
-        elif i == 2:
-            s = s.upper()
-        elif i == 3:
-            s = s.lower()
-            
-        str_arr[i] = s
+    uid, name, city, status = [s.strip() for s in string_val.split(sep)]
 
-    return divider.join(str_arr)
+    uid = f"{prefix}{uid}"
+    name = name.title().replace("_", " ")
+    city = city.upper()
+    status = status.lower()
+
+    return divider.join([uid, name, city, status])
 
 
-print(transform_str(raw_user_record, ";", " | ", " UID-"))
+print(transform_str(raw_user_record, ";", " | ", "UID-"))
